c9906183e87863e7142b7d1f6a5ab44b33e3ca2a
Gaston.Fiore <Gaston.Fiore@104a979e-f84c-11dd-8259-6defae454439>
2009-02-20 19:34:28 +0000

    Finishes implementation of Q-learning algorithm. One misterious bug still remains. Adds extra modified files needed for correct compilation.

--- hw1Code/solution.h
+++ hw1Code/solution.h
@@ -2,31 +2,6 @@
 #include <math.h>
 #include <stdint.h>
 
-/* The number of regions on the dartboard */
-#define NUM_REGIONS 7
-
-/* State */
-double current_state;
-double next_state;
-
-/* Q function */
-double Q[START_SCORE][NUM_WEDGES*NUM_REGIONS];
-
-
-
-/* Q-learning */
-
-#define ALPHA 0.9;    /* initial learning rate */
-double Qmax, next_Qmax, prev_Qmax;
-int abest;
-
-
-
-/*calculate the transition probabilities from different states*/
-double transition();
-
-
-
 /* Miscellaneous functions */
 
 /* max function */
