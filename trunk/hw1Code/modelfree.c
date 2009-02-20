cbb97c71d3f18899f484aa9496cadb5afd730070
gaston.fiore <gaston.fiore@104a979e-f84c-11dd-8259-6defae454439>
2009-02-20 06:37:24 +0000

    Continues development of Q-learning algorithm.

--- trunk/hw1Code/modelfree.c
+++ trunk/hw1Code/modelfree.c
@@ -4,64 +4,64 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "darts.h"
 #include "solution.h"
 
 location start_game() {
+    int i, a;
   location result;
   
   result.wedge = NUM_WEDGES;
   result.ring = MIDDLE_RING;
 
   /* Initialize variables used in Q-learning algorithm */
-  for (int i = 0; i < START_SCORE; i++) {
-      for (int a = 0; a < NUM_WEDGES*NUM_REGIONS; a++) {
+  for (i = 0; i < START_SCORE; i++) {
+      for (a = 0; a < NUM_WEDGES*NUM_REGIONS; a++) {
           Q[i][a] = 0;
       }
   }
   Qmax = 0;
   abest = 0;
   next_Qmax = 0;
+  prev_Qmax = 0;
 
   return result;
 }
 
-location get_target(int score, location prev_loc) {
-    location result;
+location get_target(int score, location prev_loc)
+{
+    int a, j, r;
+    location aim, result;
+    div_t ans;
 
-    if (score>NUM_WEDGES) {
-        result.wedge = NUM_WEDGES;
-        result.ring = MIDDLE_RING;
-    } else {
-        result.wedge = score;
-        result.ring = SECOND_PATCH;
-    }
-
-
-
-    for (int a = 0; a < NUM_WEDGES*NUM_REGIONS; a++) {
-        if (Q[i][a] > Qmax) {
+    prev_Qmax = Qmax;
+    for (a = 1; a <= NUM_WEDGES*NUM_REGIONS; a++) {
+        if (Q[score][a] > Qmax) {
             abest = a;
-            Qmax = Q[i][a];
+            Qmax = Q[score][a];
         }
     }
-    location aim;
-    div_t ans = div(a,NUM_REGIONS);
+    if (Qmax == prev_Qmax) {
+        abest = (rand() % (NUM_WEDGES*NUM_REGIONS)) + 1;
+    }
+    ans = div(a,NUM_REGIONS);
     if (ans.rem == 0) {
         aim.ring = (ring)NUM_REGIONS;
     } else {
         aim.ring = (ring)ans.rem;
     }
     aim.wedge = ans.quot + 1;
-    location result = throw(aim);
-    int j = i - location_to_score(result);
-    int r = j == 0 ? 1 : 0;
-    for (int a = 0; a < NUM_WEDGES*NUM_REGIONS; a++) {
-        next_Qmax = max(Q[j][a], nextQmax);
+    result = throw(aim);
+    j = score - location_to_score(result);
+    r = (j == 0) ? 1 : 0;
+    for (a = 0; a < NUM_WEDGES*NUM_REGIONS; a++) {
+        if (Q[j][a] > next_Qmax) {
+            next_Qmax = Q[j][a];
+        }
     }
-    Q[i][a] = (1 - alpha) * Q[i][a] + alpha * (r + next_Qmax);
-    i = j;
-    alpha -= alpha0 * 1/1e6;
+    Q[score][abest] = (1 - ALPHA) * Q[score][abest] + ALPHA * (r + next_Qmax);
+    score = j;
 
     return result;
 }
