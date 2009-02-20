f1227a2d96931221573c90adfd8dcf75f1fa7227
Gaston.Fiore <Gaston.Fiore@104a979e-f84c-11dd-8259-6defae454439>
2009-02-20 08:41:07 +0000

    Fixes bugs in Q-learning algorithm. Testing still underway.

--- trunk/hw1Code/modelfree.c
+++ trunk/hw1Code/modelfree.c
@@ -9,22 +9,10 @@
 #include "solution.h"
 
 location start_game() {
-    int i, a;
   location result;
   
   result.wedge = NUM_WEDGES;
   result.ring = MIDDLE_RING;
-
-  /* Initialize variables used in Q-learning algorithm */
-  for (i = 0; i < START_SCORE; i++) {
-      for (a = 0; a < NUM_WEDGES*NUM_REGIONS; a++) {
-          Q[i][a] = 0;
-      }
-  }
-  Qmax = 0;
-  abest = 0;
-  next_Qmax = 0;
-  prev_Qmax = 0;
 
   return result;
 }
@@ -32,20 +20,21 @@ location start_game() {
 location get_target(int score, location prev_loc)
 {
     int a, j, r;
+    double alpha;
     location aim, result;
     div_t ans;
 
     prev_Qmax = Qmax;
-    for (a = 1; a <= NUM_WEDGES*NUM_REGIONS; a++) {
+    for (a = 0; a < NUM_WEDGES*NUM_REGIONS; a++) {
         if (Q[score][a] > Qmax) {
-            abest = a;
+            abest = a + 1;
             Qmax = Q[score][a];
         }
     }
     if (Qmax == prev_Qmax) {
         abest = (rand() % (NUM_WEDGES*NUM_REGIONS)) + 1;
     }
-    ans = div(a,NUM_REGIONS);
+    ans = div(abest,NUM_REGIONS);
     if (ans.rem == 0) {
         aim.ring = (ring)NUM_REGIONS;
     } else {
@@ -54,13 +43,17 @@ location get_target(int score, location prev_loc)
     aim.wedge = ans.quot + 1;
     result = throw(aim);
     j = score - location_to_score(result);
+    if (j < 0) {
+      j = score;
+    }
     r = (j == 0) ? 1 : 0;
     for (a = 0; a < NUM_WEDGES*NUM_REGIONS; a++) {
         if (Q[j][a] > next_Qmax) {
             next_Qmax = Q[j][a];
         }
     }
-    Q[score][abest] = (1 - ALPHA) * Q[score][abest] + ALPHA * (r + next_Qmax);
+    alpha = ALPHA;
+    Q[score][abest] = (1 - alpha) * Q[score][abest] + alpha * (r + next_Qmax);
     score = j;
 
     return result;
